fbdf1adba2fd868a0efcca870726572021e36dfb
hafiszhfs <100256227+hafiszhfs@users.noreply.github.com>
2022-04-02 09:27:36 +0700

    Update config.js

--- config.js
+++ config.js
@@ -18,7 +18,7 @@ global.APIKeys = {
 }
 
 // Other
-global.owner = ['6283167714830','6281382420312','6288292024190']
+global.owner = [' 6281393227915','6281382420312','6288292024190']
 global.premium = ['6288292024190']
 global.packname = 'Hisoka Morrow'
 global.author = 'WhatsApp Bot'
